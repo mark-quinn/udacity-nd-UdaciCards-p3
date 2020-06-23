2c5c0f20895be0ced78ed4d67406ecf84ad74154
mark-quinn <markquinn97@hotmail.com>
2020-06-24 00:01:32 +0100

    add: actions, middleware & reducers for add Deck

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import NewDeck from './components/NewDeck';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import middleware from './middleware';
 
 export default function App() {
   return (
-    <View>
-      <NewDeck />
-    </View>
+    <Provider store={createStore(reducer, middleware)}>
+      <View>
+        <NewDeck />
+      </View>
+    </Provider>
   );
 }
