9b58d9f7766f7b6aa550474912ef8d3cb4f662cd
mark-quinn <markquinn97@hotmail.com>
2020-06-27 16:13:21 +0100

    add: notification logic

--- App.js
+++ App.js
@@ -9,25 +9,34 @@ import reducer from "./reducers";
 import middleware from "./middleware";
 import Deck from "./components/Deck";
 import { setInitialData } from "./utils/api";
-import AddQuestion from './components/AddQuestion';
-import Quiz from './components/Quiz';
-import Home from './components/Home';
+import AddQuestion from "./components/AddQuestion";
+import Quiz from "./components/Quiz";
+import Home from "./components/Home";
+import { setLocalNotification } from "./utils/notifications";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  setInitialData();
-  return (
-    <Provider store={createStore(reducer, middleware)}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Decks">
-          <Stack.Screen name="Decks" component={Home} />
-          <Stack.Screen name="New" component={NewDeck} />
-          <Stack.Screen name="Deck" component={Deck} />
-          <Stack.Screen name="Add" component={AddQuestion} />
-          <Stack.Screen name="Quiz" component={Quiz} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    setInitialData();
+    setLocalNotification();
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducer, middleware)}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Decks">
+            <Stack.Screen name="Decks" component={Home} />
+            <Stack.Screen name="New" component={NewDeck} />
+            <Stack.Screen name="Deck" component={Deck} />
+            <Stack.Screen name="Add" component={AddQuestion} />
+            <Stack.Screen name="Quiz" component={Quiz} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    );
+  }
 }
+
+export default App;
